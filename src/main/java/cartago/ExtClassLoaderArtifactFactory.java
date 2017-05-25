6d65bbeb74873f6d890dcf131fb1e5009ad9fb07
Igor C. A. de Lima <igorcadelima@gmail.com>
2017-05-25 15:47:25 -0300

    Implement ExtClassLoaderArtifactFactory#createArtifact

--- src/main/java/cartago/ExtClassLoaderArtifactFactory.java
+++ src/main/java/cartago/ExtClassLoaderArtifactFactory.java
@@ -1,5 +1,8 @@
 package cartago;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class ExtClassLoaderArtifactFactory extends ArtifactFactory {
 
 	private ClassLoader cloader;
@@ -22,7 +25,26 @@ public class ExtClassLoaderArtifactFactory extends ArtifactFactory {
 	@Override
 	public Artifact createArtifact(Class<?> caller, String templateName, Class<?>[] paramsTypes)
 			throws CartagoException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Class<?> callee = cloader.loadClass(templateName);
+
+			if ((caller == WorkspaceKernel.class) || (caller == WorkspaceArtifact.class)
+					|| (accessible(caller, callee, paramsTypes))) {
+				Constructor<?> defaultConstructor = callee.getDeclaredConstructor();
+				defaultConstructor.setAccessible(true);
+				return (Artifact) defaultConstructor.newInstance();
+			}
+
+			String errorMsg = String.format("%s can not access %s's init method", caller.getName(), callee.getName());
+			throw new IllegalAccessException(errorMsg);
+		} catch (ClassNotFoundException ex) {
+			throw new CartagoException("Template not found: " + templateName + " with " + cloader);
+		} catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
+			throw new CartagoException(e.getMessage());
+		} catch (IllegalArgumentException | NoSuchMethodException e) {
+			throw new CartagoException("No init method signature with the given parameters in " + templateName);
+		} catch (SecurityException e) {
+			throw new CartagoException("Security violation");
+		}
 	}
 }
