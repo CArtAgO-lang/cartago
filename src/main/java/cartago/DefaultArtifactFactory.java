cd9ff99a0d71217763da600268906cfc8faad347
Cleber Jorge Amaral <cleberjamaral@gmail.com>
2018-11-04 22:49:17 -0200

    Experimental version with dynamic compiler for use artifacts (the ones that does not belong to package 'cartago' and usually comes from src/env/ folder)

--- src/main/java/cartago/DefaultArtifactFactory.java
+++ src/main/java/cartago/DefaultArtifactFactory.java
@@ -1,14 +1,38 @@
 package cartago;
 
+import java.io.File;
+import java.io.FileInputStream;
+import net.openhft.compiler.CachedCompiler;
+
 public class DefaultArtifactFactory extends ArtifactFactory {
-	
+
+	private static final long serialVersionUID = -7797272818138479745L;
+	private CachedCompiler cc;
+
 	public DefaultArtifactFactory(){
 		super("default");
 	}
 	
 	public Artifact createArtifact(String templateName) throws CartagoException {
 		try {
-			Class cl = Class.forName(templateName);
+			Class<?> cl = null;
+			if (templateName.startsWith("cartago.")) {
+				cl = Class.forName(templateName);
+			} else {
+				String filename = "src/env/" + templateName.split("\\.")[0] + "/" + templateName.split("\\.")[1] + ".java";
+				File file = new File(filename);
+				FileInputStream fis = new FileInputStream(file);
+				byte[] data = new byte[(int) file.length()];
+				fis.read(data);
+				fis.close();
+				
+				String javacode = new String(data, "UTF-8");
+				
+				ClassLoader cloader = new ClassLoader() {};
+				cc = new CachedCompiler(null, null);
+				
+				cl = cc.loadFromJava(cloader, templateName, javacode);
+			}
 			return (Artifact)cl.newInstance();
 		} catch(Exception ex){
 			throw new CartagoException("Template not found: "+templateName);
