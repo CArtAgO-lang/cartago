ea22550a84c37fdd522c75d9f2f91bd7079e4a9c
Cleber Jorge Amaral <cleberjamaral@gmail.com>
2018-11-10 22:05:52 -0200

    Version with dynamic class, tested and working properly on finantialAgents project

--- src/main/java/cartago/DefaultArtifactFactory.java
+++ src/main/java/cartago/DefaultArtifactFactory.java
@@ -1,5 +1,10 @@
 package cartago;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+import net.openhft.compiler.CachedCompiler;
+
 public class DefaultArtifactFactory extends ArtifactFactory {
 	
 	public DefaultArtifactFactory(){
@@ -8,10 +13,29 @@ public class DefaultArtifactFactory extends ArtifactFactory {
 	
 	public Artifact createArtifact(String templateName) throws CartagoException {
 		try {
-			Class cl = Class.forName(templateName);
-			return (Artifact)cl.newInstance();
-		} catch(Exception ex){
-			throw new CartagoException("Template not found: "+templateName);
+			Class cl;
+			if (templateName.substring(0, 8).equals("cartago.")) {
+				cl = Class.forName(templateName);
+			} else {
+				String filename = "src/env/" + templateName.replace(".", "/") + ".java";
+
+				File file = new File(filename);
+				FileInputStream fis = new FileInputStream(file);
+				byte[] data = new byte[(int) file.length()];
+				fis.read(data);
+				fis.close();
+
+				String javacode = new String(data, "UTF-8");
+
+				ClassLoader cloader = new ClassLoader() {
+				};
+				CachedCompiler cc = new CachedCompiler(null, null);
+
+				cl = cc.loadFromJava(cloader, templateName, javacode);
+			}
+			return (Artifact) cl.newInstance();
+		} catch (Exception ex) {
+			throw new CartagoException("Template not found: " + templateName);
 		}
 	}
 }
